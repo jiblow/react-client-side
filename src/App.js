30772c6eeb1266a5bed3c31444321f49320b000e
jiblow <sanniajibola001@gmail.com>
2019-02-25 18:38:40 -0500

    second commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Navbars from './components/Navbars';
+
+
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <Navbars />
+      </div>
+   
+    );
+  }
+}
+
+export default App;
